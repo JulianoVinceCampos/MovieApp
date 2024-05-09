fa6bb399e4fd009be718fc4a0e0f59f2df4435b4
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 13:53:59 -0300

    Adicionado módulo NetWorkModule para fornecer dependências relacionadas à rede. Este módulo inclui métodos de fornecimento para interceptadores de parâmetros e de registro HTTP, cliente OkHttpClient, fábrica de conversão GsonConverterFactory e serviço MovieService. O cliente OkHttpClient é configurado com interceptadores para adicionar parâmetros às solicitações e para registrar as solicitações e respostas HTTP. O serviço MovieService é criado usando Retrofit, com a base URL definida em BuildConfig.BASE_URL.

---
+++ app/src/main/java/br/com/julianovincedecampos/core/di/NetWorkModule.kt
@@ -0,0 +1,71 @@
+package br.com.julianovincedecampos.core.di
+
+import br.com.br.com.julianovincedecampos.BuildConfig
+import br.com.julianovincedecampos.core.data.remote.MovieService
+import br.com.julianovincedecampos.core.data.remote.ParamsInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetWorkModule {
+
+    private const val TIMEOUT_SECONDS = 15L
+
+    @Provides
+    fun provideParamsInterceptor(): ParamsInterceptor {
+        return ParamsInterceptor()
+    }
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            )
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        paramsInterceptor: ParamsInterceptor,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(paramsInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideMovieService(
+        client: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): MovieService {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(converterFactory)
+            .build()
+            .create(MovieService::class.java)
+    }
+
+}
