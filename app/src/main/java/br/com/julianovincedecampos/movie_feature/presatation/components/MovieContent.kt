192d2bb0bcb6160d28654523f2e7e443713d5993
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 11:47:02 -0300

    Adicionado componente MovieContent para exibir uma lista de filmes paginada. Este componente é composto por um LazyVerticalGrid que mostra os filmes em colunas fixas de 3 itens. Cada item é representado pelo componente MovieItem. Ele recebe uma lista de filmes paginados (LazyPagingItems<Movie>), um Modificador, valores de padding e uma função de clique para lidar com a interação do usuário.

---
+++ app/src/main/java/br/com/julianovincedecampos/movie_feature/presatation/components/MovieContent.kt
@@ -0,0 +1,45 @@
+package br.com.julianovincedecampos.movie_feature.presatation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import br.com.julianovincedecampos.core.domain.model.Movie
+
+@Composable
+fun MovieContent(
+    modifier: Modifier = Modifier,
+    pagingMovies: LazyPagingItems<Movie>,
+    paddingValues: PaddingValues,
+    onClick: (movieId: Int) -> Unit
+) {
+    Box(modifier = modifier.background(Color.Black)) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = paddingValues,
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(pagingMovies.itemCount) { index ->
+                val movie = pagingMovies[index]
+                movie?.let {
+                    MovieItem(
+                        voteAverage = it.voteAverage,
+                        imageUrl = it.imageUrl,
+                        id = it.id,
+                        onClick = { movieId -> onClick(movieId) })
+                }
+            }
+        }
+    }
+}
