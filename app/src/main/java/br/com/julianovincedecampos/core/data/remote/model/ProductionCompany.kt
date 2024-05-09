950d77091d3bf24f1da87ca6afb5a285c563a30c
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 13:55:19 -0300

    Adicionada classe de modelo ProductionCompany, que representa uma empresa de produção de filmes. Esta classe possui propriedades para o ID da empresa, o caminho do logo, o nome e o país de origem. As anotações SerializedName são usadas para fazer o mapeamento correto dos campos ao converter o JSON.

---
+++ app/src/main/java/br/com/julianovincedecampos/core/data/remote/model/ProductionCompany.kt
@@ -0,0 +1,10 @@
+package br.com.julianovincedecampos.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductionCompany(
+    val id: Int,
+    @SerializedName("logo_path") val logoPath: String,
+    val name: String,
+    @SerializedName("origin_country") val originCountry: String
+)
