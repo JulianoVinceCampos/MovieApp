dc76ae96af7b1879b861234d31ac21ddbd01c5a1
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 13:56:07 -0300

    Adicionada classe de modelo ProductionCountry, que representa um país de produção de filmes. Esta classe possui propriedades para o código ISO 3166-1 do país e seu nome. As anotações SerializedName são usadas para fazer o mapeamento correto dos campos ao converter o JSON.

---
+++ app/src/main/java/br/com/julianovincedecampos/core/data/remote/model/ProductionCountry.kt
@@ -0,0 +1,8 @@
+package br.com.julianovincedecampos.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductionCountry(
+    @SerializedName("iso_3166_1") val iso31661: String,
+    val name: String
+)
