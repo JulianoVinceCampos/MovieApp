ba82692fd1ef5191bb22505026cffd94aadb37cb
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 13:58:19 -0300

    Adicionada a classe SearchResult que representa um resultado de pesquisa. Ela contém informações sobre um item pesquisado, incluindo seu título, data de lançamento, popularidade, avaliação, entre outros.

---
+++ app/src/main/java/br/com/julianovincedecampos/core/data/remote/model/SearchResult.kt
@@ -0,0 +1,38 @@
+package br.com.julianovincedecampos.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResult(
+    @SerializedName("adult")
+    val adult: Boolean,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("first_air_date")
+    val firstAirDate: String,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_name")
+    val originalName: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("overView")
+    val overView: String,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("video")
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
+)
