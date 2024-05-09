da76e6565a0bc16fd562962b2e4a2a389129b82f
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-09 13:58:59 -0300

    Adicionada a classe SpokenLanguage que representa um idioma falado em um filme. Ela contém o nome em inglês, o código ISO 639-1 e o nome completo do idioma.

---
+++ app/src/main/java/br/com/julianovincedecampos/core/data/remote/model/SpokenLanguage.kt
@@ -0,0 +1,9 @@
+package br.com.julianovincedecampos.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SpokenLanguage(
+    @SerializedName("english_name") val englishName: String,
+    @SerializedName("iso_639_1") val iso6391: String,
+    val name: String
+)
