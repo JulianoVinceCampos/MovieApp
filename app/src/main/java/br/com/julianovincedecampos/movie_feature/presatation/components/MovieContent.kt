a5937fe5ed3b5754ad24a1fe526ed1c62e1547b9
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-05-10 10:21:30 -0300

    Atualiza MovieContent para incluir tratamento de estado de carregamento e erro

--- app/src/main/java/br/com/julianovincedecampos/movie_feature/presatation/components/MovieContent.kt
+++ app/src/main/java/br/com/julianovincedecampos/movie_feature/presatation/components/MovieContent.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import br.com.julianovincedecampos.core.domain.model.Movie
+import br.com.julianovincedecampos.core.presentation.componets.common.ErrorScreen
+import br.com.julianovincedecampos.core.presentation.componets.common.LoadingView
 
 @Composable
 fun MovieContent(
@@ -38,6 +43,57 @@ fun MovieContent(
                         imageUrl = it.imageUrl,
                         id = it.id,
                         onClick = { movieId -> onClick(movieId) })
+                }
+            }
+            pagingMovies.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingView()
+                        }
+                    }
+
+                    loadState.append is LoadState.Loading -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingView()
+                        }
+                    }
+
+                    loadState.refresh is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(
+                                message = "Verifique sua conexão com a internet",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
+
+                    loadState.append is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxCurrentLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(
+                                message = "Verifique sua conexão com a internet",
+                                retry = {
+                                    retry()
+                                })
+                        }
+                    }
                 }
             }
         }
